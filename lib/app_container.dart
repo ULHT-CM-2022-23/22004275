24eb9e74a5590714c7a444428f7782d7bd2a2455
Felix <rodrifelix99@gmail.com>
2023-03-03 09:20:36 +0000

    Fixes

--- lib/app_container.dart
+++ lib/app_container.dart
@@ -1,9 +1,6 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:miniprojeto/pages/avaliacoes.dart';
 import 'package:miniprojeto/pages/create_avaliacao.dart';
-import 'package:miniprojeto/providers/avaliacao_provider.dart';
-import 'package:provider/provider.dart';
 
 import 'pages/home.dart';
 
@@ -47,55 +44,28 @@ class _AppContainerState extends State<AppContainer> {
 
   @override
   Widget build(BuildContext context) {
-    final avaliacoes = Provider.of<AvaliacaoProvider>(context).avaliacoes;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(_pageTitle()),
       ),
       body: _buildPage(),
-      bottomNavigationBar: CurvedNavigationBar(
-        index: _currentIndex,
-        height: 60.0,
-        items: <Widget>[
-          const Icon(Icons.home, size: 30, color: Colors.white),
-          // List icon with number of avaliacoes
-          Stack(
-            children: [
-              const Icon(Icons.list, size: 30, color: Colors.white),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(1),
-                  decoration: BoxDecoration(
-                    color: Colors.purple[100],
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  ),
-                  constraints: const BoxConstraints(
-                    minWidth: 16,
-                    minHeight: 16,
-                  ),
-                  child: Text(
-                    avaliacoes.length.toString(),
-                    style: const TextStyle(
-                      color: Colors.purple,
-                      fontSize: 10,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _navigate,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
           ),
-          const Icon(Icons.add, size: 30, color: Colors.white),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Listagem',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Registo',
+          ),
         ],
-        color: Colors.purple.shade400,
-        buttonBackgroundColor: Colors.purple.shade400,
-        backgroundColor: Colors.transparent,
-        animationCurve: Curves.easeInOut,
-        animationDuration: const Duration(milliseconds: 400),
-        onTap: (int index) => _navigate(index),
       ),
     );
   }
